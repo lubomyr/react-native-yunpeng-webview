ce5d685ccfc71f70af8ff7a0fa481a05e0a21ab1
Imran Sulemanji <imranolas@gmail.com>
2018-01-24 12:00:19 +0000

    Remove unnecessary dependencies

--- android/src/main/java/com/yunpeng/react/webview/YPWebViewManager.java
+++ android/src/main/java/com/yunpeng/react/webview/YPWebViewManager.java
@@ -4,12 +4,8 @@ import com.facebook.react.views.webview.ReactWebViewManager;
 import com.facebook.react.views.webview.WebViewConfig;
 import com.facebook.react.uimanager.annotations.ReactProp;
 import com.facebook.react.module.annotations.ReactModule;
-import com.facebook.react.uimanager.ThemedReactContext;
 
-import android.net.http.SslError;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.webkit.SslErrorHandler;
 import android.content.res.Resources;
 
 @ReactModule(name = YPWebViewManager.REACT_CLASS)
@@ -36,13 +32,5 @@ public class YPWebViewManager extends ReactWebViewManager {
     view.getSettings().setUseWideViewPort(enabled);
     view.getSettings().setLoadWithOverviewMode(enabled);
   }
-
-  @Override
-  protected void addEventEmitters(ThemedReactContext reactContext, WebView view) {
-    // Do not register default touch emitter and let WebView implementation handle touches
-    view.setWebViewClient(new YPWebViewClient());
-  }
-
-
 
 }
